a3330c6adbbca62f899aa41259ed9701b44bd763
drjlevi6 <drjlevi6@aol.com>
2021-09-02 10:41:47 -0400

    Update movie-view.jsx
    Removed console.log calls

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,10 +7,6 @@ export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
     
-    console.log("movie-view.movie:", movie);
-    console.log("movie-view.movie:", movie);
-
-
     return (
       <div className="movie-view">
         <div className="movie-poster">
