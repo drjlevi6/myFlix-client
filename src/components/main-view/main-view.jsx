03a9d69b7ab9f506366205161386233a2aa85456
drjlevi6 <drjlevi6@aol.com>
2021-09-07 11:57:46 -0400

    Update main-view.jsx
    Added Router code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import axios from 'axios';
 import "./main-view.scss"; /* file is currently empty, 
                             but we might need it later. */
+
+import { BrowserRouter as Router, Route } from 
+  "react-router-dom";
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from 
+  '../registration-view/registration-view';
 
 export default class MainView extends React.Component {
   constructor(){
     super();
+
     this.state = {
       movies: [],
       selectedMovie: null,
@@ -92,56 +99,43 @@ export default class MainView extends React.Component {
     /* <!--If there is no user, the LoginView is rendered. 
     If there is a user logged in, the user details are 
     *passed as a prop to the LoginView--> */
-    if (!user) {
-      return <LoginView onLoggedIn={user => 
-        this.onLoggedIn(user)} />;
-    }
+    if (!user) return
+      <Row>
+        <Col>
+          <LoginView onLoggedIn={user => 
+            this.onLoggedIn(user)} />
+        </Col>
+      </Row>
 
     // /* <!--Before the movies have been loaded
     // If the state of `selectedMovie` is not null, that 
     //  selected movie will be returned otherwise, all 
     //  *movies will be returned.--> */
-   if (movies.length === 0) {
-      // Why isn't the key assignment working? (typeof movies = object)
-      return <div></div>
-    }; // end return
+    if (movies.length === 0) {
+      return <div className="main-view" />
+    }; // end if
 
     return (
-    //  <div className="main-view">
-      <Container>
-        {selectedMovie ? 
-            <Row className="justify-content-md-center">
-              <Col xs={12}>
-                <MovieView movie={selectedMovie}
-                  onBackClick={newSelectedMovie => {
-                    this.setSelectedMovie(newSelectedMovie) 
-                  }}/>
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route exact path="/" render={() => {
+            return movies.map(m => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
               </Col>
-            </Row>
-          : (
-              <Row className="justify-content-md-center">
-                {movies.map(movie => (
-                  <Col xs={4} key={movie._id} >
-                    <MovieCard
-                      movie={movie}
-                      title={movie.title}
-                      imagePath={movie.imagePath}
-                      onMovieClick={(newSelectedMovie) => {
-                        this.setSelectedMovie(newSelectedMovie); }} />
-                  </Col>
-                ))}
-              </Row>
-            )
-        }
-        <Button variant="dark" 
-          onClick={() => { this.onLoggedOut() }}>
-            Logout</Button>
-        </Container>
-    //  </div>
-    )
-
-  } // end if
-} // end render
+            ))
+          }} />
+          <Route path="/movies/:movieId" render={({ match }) => {
+            return 
+              <Col md={8}>
+                <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+              </Col>
+          }} />
+        </Row>
+      </Router>
+    );
+  }
+}
 
 // There's no MainView.propTypes because no props are passed to MainView
 // during instantiation.
