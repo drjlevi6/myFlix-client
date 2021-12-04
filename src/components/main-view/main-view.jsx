bdab14ff61df9aeb528d4e5677bb5d24e5ddd3ed
drjlevi6 <drjlevi6@aol.com>
2021-12-04 12:52:04 -0500

    Update main-view.jsx
    Multiple modifications to filterMovieCards(), sortMoviesByName(), render() such that filtering, sorting, reverting now all appear to work. Code still could se cleaning.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,1 +1,336 @@
-/Users/jonathan/Documents/GitHub/portfolio_website/myFlix-client/prev/20211202-105857/main-view.jsx
+import React, { StrictMode } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import "./main-view.scss";
+
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import { GenreView } from '../genre-view/genre-view';
+import { DirectorView } from '../director-view/director-view';
+import { ProfileView } from '../profile-view/profile-view';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { render } from 'react-dom';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { scrollLeft } from 'dom-helpers';
+
+var mainView;
+
+export default class MainView extends React.Component {
+  constructor(){
+    super();
+    localStorage.clear();
+    this.state = { movies: [], user: null, search_string_low: '',
+      do_sort: false, modified_movies: [] };
+    mainView = this;
+    window.addEventListener('resize', this.adjustTopControlsRowHeight);
+  }
+
+  // Filter movie-cards per user search string  (case-insensitive)
+  filterMovieCards = (e) => {
+    const {movies, search_string_low, do_sort} = this.state;
+    var {modified_movies} = this.state;
+    let filtered_movies = movies.filter(
+      movie => movie.title.toLowerCase().includes(search_string_low)
+    )
+    modified_movies = filtered_movies;
+    this.setState( {
+        search_string_low: e.target.value.toLowerCase(),
+        do_sort: false,
+        modified_movies: filtered_movies
+      });
+  }
+
+  // Sort all movies by title (case-insensitive)
+  // We do the sorting in render()'s "else if (do_sort)" case:
+  // If we did the sorting here and set do_sort to false,
+  // using render()'s "else" case would conflict with the needs of the link
+  // from profile-view's "All Movies" button.
+  sortMoviesByTitle = () => {
+    const { search_string_low, do_sort }  = this.state;
+    this.setState({ search_string_low: '', do_sort: true });
+  }
+
+  revertMovieCards = () => {
+    const { movies, search_string_low, do_sort, modified_movies } = this.state;
+    this.setState({
+      search_string_low: '', do_sort: false,
+      modified_movies: deepCopy(movies)
+    });
+  }
+  
+// Adjust top of movie-cards dynamically, according to height of top row.
+  adjustTopControlsRowHeight() {
+    let all_top_text_and_controls_rows = 
+      document.getElementsByClassName('top-text-and-controls-row');
+    if (all_top_text_and_controls_rows.length > 0) {
+      let top_text_controls_row = all_top_text_and_controls_rows[0];
+      let top_row_height = window.getComputedStyle(top_text_controls_row).height;
+      let cards_row = document.getElementsByClassName('movie-cards-row')[0];
+      cards_row.style.top = top_row_height;
+    }
+  } 
+
+  moveBottomButtonsDiv(oldWinHeight) {
+    return window.innerHeight;
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.location.pathname="/";
+  }
+
+  /* When a movie is clicked, this function is invoked and 
+    updates the state of the `selectedMovie` *property 
+    to that movie */
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  /* When a user successfully logs in, this function updates 
+  the `user` property in state to that *particular user*/
+
+  onLoggedIn(authData) {
+    var mainView = this;
+    let User = authData.user.Username;
+    this.setState({
+      user: User
+    });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('favorite_ids', authData.user.FavoriteMovies);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  getMovies(token) {
+    axios.get('https://drjs-myflix-app.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {     // response.data is the array of movies
+      // Assign the result to the state (jl: and also localStorage)
+       this.setState({
+        movies: response.data
+      });
+    })
+   .catch(function (error) {
+      console.log(error);
+    });
+   }
+
+  render() {  // React allows "className" in <div>s! 
+    const { movies, user, search_string_low, do_sort } = this.state;
+    var {modified_movies} = this.state;
+
+    if(search_string_low) {
+      modified_movies = movies.filter(m =>
+          m.title.toLowerCase().includes(search_string_low)
+      )
+    } else if (do_sort) {
+      modified_movies = deepCopy(movies).sort((m,n) =>  
+        (m.title.toLowerCase() < n.title.toLowerCase()) ? -1 : 1)
+    } else {
+      modified_movies = deepCopy(movies);
+    }
+
+    /* <!--If there is no user, the LoginView is rendered. 
+    If there is a user logged in, the user details are 
+    *passed as a prop to the LoginView--> */
+
+    if (!user) {
+      return (
+        <Row>
+          <Router>
+          <Col>
+            <LoginView onLoggedIn={user => 
+              this.onLoggedIn(user)} />
+          </Col>
+          </Router>
+        </Row>
+      )
+    }
+
+    // /* <!--Before the movies have been loaded
+    // If the state of `selectedMovie` is not null, that 
+    //  selected movie will be returned otherwise, all 
+    //  *movies will be returned.--> */
+
+    if (movies.length === 0) {
+       return <div className="main-view" />
+    }
+    let filter_sort_column_width = 6;
+    return (
+      <Container className='router-container'>
+        <Router className='router'>
+          <Row className="main-view-row justify-content-md-center">
+            <Route exact path="/" render={() => {
+              if (!user) return 
+                <Col>
+                  <LoginView onLoggedIn={
+                    user => this.onLoggedIn(user)
+                  } />
+                </Col>
+              return( 
+                <div>
+                  <Row className='top-text-and-controls-row'>
+                    <Col className='filter-sort-controls-column' 
+                        xs={filter_sort_column_width}>
+                      <Row className='filter-sort-text-row'>
+                        <h5 className='filter-sort-text'>
+                          Filter/Sort Movies By Name:
+                        </h5>
+                      </Row>
+                      <Row className='filter-sort-controls-row'>
+                        <Col className='filter-input-group-column' xs={8}>
+                          <InputGroup className='filter-input-group'>
+                            <InputGroup.Text className='input-group-filter-text'>
+                              Filter
+                            </InputGroup.Text>
+                            <FormControl className='filter-textarea' id='searchForm'
+                              type='text' placeholder="Movie Name" 
+                                onChange={this.filterMovieCards}
+                             />
+                          </InputGroup>
+                        </Col>
+                        <Col className='sort-button-column'>
+                          <Button className='sort-button' type='button'
+                            onClick={this.sortMoviesByTitle}>
+                              Sort
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col className='return-to-profile-column'
+                        xs={3}>
+                      <Row className='return-to-profile-row'>
+                        <Button className='profile-button' variant='dark'
+                          onClick={() => history.back()}>
+                            Return To Profile
+                        </Button>
+                      </Row>
+                      <Row className='revert-row'>
+                        <Button className='button' variant='dark'
+                          onClick={this.revertMovieCards}>
+                            Revert Cards
+                        </Button>
+                      </Row>
+                   </Col>
+                  </Row>                  
+                  <Row className='movie-cards-row'>
+                    {modified_movies.map(m => (
+                      <Col xs={7} sm={5} md={4} lg={3} key={m._id}>
+                        <MovieCard movie={m} />
+                      </Col>
+                    ))}
+                  </Row>
+                </div>
+              ) // e
+            }} />
+            <Route path="/register" render={() => {
+              return <Col>
+                <RegistrationView />
+              </Col>
+            }} />
+
+            <Route exact path="/movies/:movieId"
+              render={({ match, history }) => {
+                return (
+                  <Col md={8}>
+                    <MovieView 
+                      movie={movies.find(
+                          m => (m._id === match.params.movieId)
+                      )} 
+                      onBackClick={() => history.push("/user")}
+                    />
+                  </Col>
+                )
+            }} />       
+            <Route exact path="/user"
+              render={({ match, history }) => {
+                return (
+                  <Col>
+                    <ProfileView movies={this.state.movies}
+                    />
+                  </Col>
+                )
+            }} />       
+            <Route exact path="/movies/genre/:title"
+              render={({ match, history }) => {
+                let mTitle = match.params.title;
+                return (
+                  <Col md={8}>
+                    <GenreView 
+                      genre={movies.find(
+                          m => ( m.title === mTitle )
+                      ).genre}
+                      movie={movies.find(  m => (m.title === mTitle))} 
+                      onBackClick={() => history.goBack()}/>
+                  </Col>
+                )
+            }} />  
+            <Route exact path='/movies/director/:title'   
+              render={({ match, history }) => {
+                let mTitle = match.params.title;
+                return (
+                  <Col md={8}>
+                    <DirectorView
+                      movie={movies.find( m => (m.title === mTitle))}
+                      onBackClick={() => history.goBack()}
+                    />
+                  </Col>
+                )
+            }} />  
+          </Row>
+          <div className='bottom-buttons-div'>
+            <Row className="back-button-row">
+              <Col className="d-grid">
+                <Button className="back-button"
+                  variant="dark" onClick={() => history.back()}>
+                  Go Back
+                </Button>
+              </Col>
+              <Col className="d-grid">
+                <Button className="logout-button" variant="dark"
+                onClick={this.onLoggedOut}>
+                  Log Out
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        </Router>
+      </Container>
+    );  // end return
+  } // end if
+} // end class
+
+// deepCopy: deep clone of a "JSON-able" array (specifically, the movies array)
+// Thanks Atta-Ur_Rehman Shah,
+//   https://attacomsian.com/blog/javascript-deep-clone-array
+function deepCopy(json_able) {
+  return JSON.parse(JSON.stringify(json_able));
+}
+
+// There's no MainView.propTypes because no props are passed to MainView
+// during instantiation.
