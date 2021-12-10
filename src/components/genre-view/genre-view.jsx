e1c379f60526a1ad5ab800b4b3c350d94accdcb4
drjlevi6 <drjlevi6@aol.com>
2021-12-10 17:05:12 -0500

    Update genre-view
    Added buttons and styling

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { Link } from "react-router-dom";
 import { scrollParent } from 'dom-helpers';
 import { Alert } from 'react-bootstrap';
@@ -18,20 +19,35 @@ export class GenreView extends React.Component {
     render() {
       const { movie, onBackClick } = this.props;
       console.log(this.props);
-
-      return (
+      console.log(this.state);
+      return(
         <Container className="movie-view">  
             <Row className="movie-poster-row">
-              <Col xs={6}>
-              <img crossOrigin="anonymous" src={movie.imagePath} />
+              <Col xs={3}>
+              <Button variant='dark'>
+              &lt;&mdash;Movie
+                  </Button>
+              </Col>
+              <Col xs={4}>
+                <Alert className='data-area' variant='info'>
+                <Alert.Heading>{movie.genre.name}</Alert.Heading>
+                </Alert>
+              </Col>
+              <Col xs={5}>
+                <ButtonGroup className="movies-profile-buttons" 
+                  aria-label="All-Movies and Profile Buttons">
+                  <Button>All Movies</Button>&nbsp; <Button>Profile</Button>
+                </ButtonGroup>
               </Col>
             </Row>
             <Alert className='data-area' variant='info'>
-              <Alert.Heading>Title: {movie.title}</Alert.Heading>
-              <Alert.Heading>Genre: {movie.genre.name}</Alert.Heading>
+              <Alert.Heading>Description:</Alert.Heading>
               <p>{movie.genre.description}</p>
             </Alert>
-     </Container>
-      );
-     }
+            <Row className='matching-movies-text-row'>
+              <h6>Some movies with this genre are:</h6>
+            </Row>
+        </Container>
+      )
+    }
 }
