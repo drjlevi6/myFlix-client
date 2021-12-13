6b579f1b66c3d64e85e8f10e0b9a8fcfe5cca31b
drjlevi6 <drjlevi6@aol.com>
2021-12-13 13:20:21 -0500

    Created new director-view.js, .scss
    These are essentially copies of genre-view.jsx and .scss; need modification.

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,41 +1,78 @@
-import React, { StrictMode } from 'react';
-import PropTypes from 'prop-types';
-
+import './director-view.scss';
+import { MovieCard } from '../movie-card/movie-card';
+import {setMovieCardsTop} from 
+  '../common_components/common_components';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { Alert } from 'react-bootstrap';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { Link } from "react-router-dom";
+import { scrollParent } from 'dom-helpers';
+import { Alert } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 export class DirectorView extends React.Component {
-  constructor() {
+  constructor(){
     super();
   }
 
-  render() {
-    const { movie, onBackClick } = this.props;
+  componentDidMount() {
+    setMovieCardsTop(); //checked 20211211, needed
+  }
 
-    let director = movie.director;
-    return (
-      <Container className="director-view">
-        <Row className="movie-poster-row">
-          <Col xs={6}>
-          <img crossOrigin="anonymous" src={movie.imagePath} />
-          </Col>
-        </Row>
-        <Alert variant='info'>
-          <Alert.Heading>Director:</Alert.Heading>
-          <p>{director.name}</p>
-          <hr />
-          <Alert.Heading>Bio:</Alert.Heading>
-          <p>{director.bio}</p>
-          <Alert.Heading>Birth year:</Alert.Heading>
-          <p>{director.birth}</p>
-          <Alert.Heading>Death year:</Alert.Heading>
-          <p>{director.death}</p>
-        </Alert>
-      </Container>
+  render() {
+    const { movies, movie, onBackClick } = this.props;
+    let this_genres_movies = movies.filter(m => 
+      m.genre.name === movie.genre.name
     );
+    return(
+      <Container className="movie-view">  
+        <Row className='top-controls-row' id='top-controls-row'>
+          <Row className="top-buttons-row">
+            <Col className='back-to-movie-col' xs={4}>
+            <Button className='back-to-movie-button' variant='dark'
+              onClick={() => history.back()}>
+            &lt;&mdash; Movie
+                </Button>
+            </Col>
+            <Col className='genre-name-col' xs={3}>
+              <Alert className='genre-name-alert' variant='info'>
+              <Alert.Heading>{movie.genre.name}</Alert.Heading>
+              </Alert>
+            </Col>
+            <Col className='allMovies-profile-col' xs={5}>
+              <ButtonGroup className="movies-profile-buttons" 
+                aria-label="All-Movies and Profile Buttons">
+                <Button as={Link} to='/' variant='dark'>
+                  All Movies
+                </Button>&nbsp; 
+                <Button as={Link} to='/user' variant='dark'>
+                  Profile
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+          <Alert className='data-area' variant='info'>
+            <Alert.Heading>Description:</Alert.Heading>
+            <p>{movie.genre.description}</p>
+          </Alert>
+          <Row className='matching-movies-text-row'>
+            <Col className='matching-movies-text-col' 
+              xs={10} sm={7}>
+                Some movies with this genre are:
+            </Col>
+          </Row>
+        </Row>
+        <Row className='movie-cards-row'>
+          {this_genres_movies.map(m => (
+            <Col xs={7} sm={6} md={6} lg={4} key={m._id}>
+            <MovieCard movie={m} />
+            </Col>
+          ))}
+        </Row>
+    </Container>
+    )
   }
 }
