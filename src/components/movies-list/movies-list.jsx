0d4252b0da611fed19e29830655ba93d60d32981
drjlevi6 <drjlevi6@aol.com>
2022-01-03 15:12:35 -0500

    Update movies-list.jsx
    Rewrote the file per course text, changing MoviesList from class to function; added mapStateToProps

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,10 +1,32 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
 
-export class MoviesList extends React.Component {
-  constructor() {
-    super()
+import { MovieCard } from '../movie-card/movie-card';
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => 
+      m.Title.toLowerCase().
+      includes(visibilityFilter.toLowerCase()));
   }
 
+  if (!movies) return <div className="main-view"/>;
+
+ return filteredMovies.map(m => (
+    <Col md={3} key={m._id}>
+      <MovieCard movie={m} />
+    </Col>
+  ));
+}
   render() {
     return(<div> MoviesList </div>);
   }
